2eddbe4df9bb11d15be56c02ebf017d7ce4ac4b2
cy2260 <yunchao60@hotmail.com>
2013-05-26 00:29:16 -0400

    virgin

--- insertNodeIntoBST/insertNodeIntoBST.cpp
+++ insertNodeIntoBST/insertNodeIntoBST.cpp
@@ -10,3 +10,31 @@ Node* insertNodeIntoBST( Node* root, int data ) {
 
     return root;
 }
+
+Node* insertNodeIntoBST( Node* root, int data ) }
+    if( !root ) return new Node(data);
+
+    Node* currNode = root;
+
+    while( currNode ) {
+        if( data > currNode->data ) {
+	    if( currNode->right ) {
+	        currNode = currNode->right;
+	    }
+	    else {
+	        currNode->right = new Node(data);
+		break;
+	    }
+	else {
+	    if( currNode->left ) {
+	        currNode = currNdoe->left;
+	    }
+	    else {
+	        currNode->left = new Node(data);
+	        break;
+	    }
+	}
+    }
+    
+    return root;
+}
