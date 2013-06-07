4f103cf1211b08bc0119fd167faaab97813e0174
cy2260 <yunchao60@hotmail.com>
2013-06-07 01:09:37 -0400

    refactor

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -8,6 +8,7 @@ int getRightChild( int idx ) {
 int getParent( int idx ) {
     return (idx-1)/2;
 }
+
 void siftUp( int* arr, int rootIdx, int endIdx ) {
     if( !arr ) return;
 
@@ -16,17 +17,45 @@ void siftUp( int* arr, int rootIdx, int endIdx ) {
         int rightIdx = getRightChild( rootIdx );
         int swapIdx  = rootIdx;
         
+        if( arr[leftIdx] > arr[rootIdx] ) {
+            swapIdx = leftIdx;
+        } 
+        if( rightIdx <= endIdx && arr[rightIdx] > arr[swapIdx] ) {
+            swapIdx = rightIdx;
+        }
+        
+        if( rootIdx == swapIdx ) {
+            break; //root is the max value
+        }
+        else {
+            swap( arr[rootIdx], arr[swapIdx] );
+            rootIdx = swapIdx; //check if its subtree satisfy heap property
+        }
     }    
 }
 
-void maxHeapify( int* arr, int len ) {
+void maxHeapify( int* arr, int startIdx, int endIdx ) {
     if( !arr ) return;
     
     //this is the last node that has child/children
     //last element is either its left child or right child
-    int rootIdx = ( len - 1 ) / 2;
+    int rootIdx = ( endIdx - startIdx ) / 2;
     while( rootIdx >= 0 ) {
-        siftUp( arr, rootIdx, len-1 );
+        siftUp( arr, rootIdx, endIdx );
         --rootIdx;
     }
 }
+
+void heapSort( int* arr, int len ) {
+    if( !arr ) return;
+    
+    maxHeapify( arr, 0, len-1 );
+    
+    int i = len - 1;
+    while( i > 0 ) {
+        swap( arr[i], arr[0] );
+        --i;
+        siftUp( arr, 0, i );
+    }
+}
+
