c0cfaa6a8c7d3d79abf5f59773225a58cd12e7b2
cy2260 <yunchao60@hotmail.com>
2013-06-09 10:29:29 -0400

    virgin

---
+++ cplusplus/cplusplus.cpp
@@ -0,0 +1,45 @@
+class Foo
+{
+public:
+    Foo();
+    Foo( int n );
+    ~Foo();
+    Foo( const Foo& f );
+    Foo& operator=(const Foo& f );
+
+private:
+    int* dataPtr;
+};
+
+Foo::Foo() {
+    dataPtr = new int;
+}
+
+Foo::Foo( int n ) {
+    dataPtr = new int;
+    *dataPtr = n;
+}
+
+Foo::~Foo() {
+    delete dataPtr;
+}
+
+Foo::Foo( const Foo& f ) {
+    dataPtr = new int;
+    *dataPtr = *(f.dataPtr);
+}
+
+Foo& Foo::operator=( const Foo& f ) {
+    //self assignment, do nothing
+    if( this == &f ) {
+        return *this;
+    }
+    
+    //release old resource
+    delete dataPtr;
+    //acquire new resource
+    dataPtr = new int;
+    *dataPtr = *(f.dataPtr);
+
+    return *this;
+}
