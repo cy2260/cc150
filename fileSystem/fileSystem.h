6ff109d22e0bcf0b828564680f81b56e55796978
cy2260 <yunchao60@hotmail.com>
2013-06-11 22:35:47 -0400

    virgin

---
+++ fileSystem/fileSystem.h
@@ -0,0 +1,74 @@
+class Entry
+{
+public:
+    Entry( const string& n, Directory* p )
+    : name( n )
+    , parent( p )
+    {}
+    ~virtual Entry();
+    
+    string getFullPath() {
+        return parent->getFullPath() + "/" + name;
+    }
+
+    virtual int size() = 0; //file size differs from dir size
+   
+    bool delete() {
+        if( parent == NULL ) return false; //cannot delete root
+	return parent->deleteEntry( this );
+    }
+
+    void rename( const string& newName );
+    string getName();
+    
+    int getCreationTime();
+    int getLastUpdatedTime();
+    int getLastAccessedTiem();
+
+protected:
+    string name;
+    Directory* parent;
+    int creationTime;
+    int lastUpdatedTime;
+    int lastAccessedTime;
+};
+
+class File : public Entry
+{
+public:
+    File( const string& n, Directory* p, int sz )
+    : Entry( n, p )
+    , size = sz
+    {}
+    ~File();
+
+    int size() { return size; }
+    string getContent();
+    void setContent( const string& content );
+protected:
+    string content;
+    int size;
+};
+
+class Directory : public Entry
+{
+public:
+    Directory( const string& n, Directory* p )
+    : Entry( n, p )
+    {}
+
+    int size() {
+        int size = 0;
+	for( int i = 0; i < entries.size(); ++i ) {
+	    size += entries[i]->size();
+	}
+	return size;
+    }
+
+    void listAll();
+    void listEntry( const string& name );
+
+protected:
+    vector<Entry*> entries;
+
+};
